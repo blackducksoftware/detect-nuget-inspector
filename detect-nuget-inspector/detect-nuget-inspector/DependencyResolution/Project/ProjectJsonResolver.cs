be7a1636160f8676b1c04a695a7dbcd535319d7e
jraihan <jahid.raihan@dsinnovators.com>
2023-06-06 17:27:00 +0600

    implemented generic dependency extraction from project.json for all dotnet versions.

--- detect-nuget-inspector/detect-nuget-inspector/DependencyResolution/Project/ProjectJsonResolver.cs
+++ detect-nuget-inspector/detect-nuget-inspector/DependencyResolution/Project/ProjectJsonResolver.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json.Linq;
+using NuGet.LibraryModel;
+using NuGet.ProjectModel;
 
 namespace Synopsys.Detect.Nuget.Inspector.DependencyResolution.Project
 {
@@ -19,19 +17,70 @@ namespace Synopsys.Detect.Nuget.Inspector.DependencyResolution.Project
 
         public DependencyResult Process()
         {
+            ProjectJsonResolver projectJsonResolver = new ProjectJsonResolver(ProjectName, ProjectJsonPath);
             var result = new DependencyResult();
 
-            NuGet.ProjectModel.PackageSpec model = NuGet.ProjectModel.JsonPackageSpecReader.GetPackageSpec(ProjectName, ProjectJsonPath);
-            IList<NuGet.LibraryModel.LibraryDependency> packages = model.Dependencies;
+            JObject packageSpecDependencies = projectJsonResolver.ExtractPackageSpecDependencies(ProjectJsonPath, projectJsonResolver);
+            PackageSpec packageSpec = projectJsonResolver.CreatePackageSpecFromJson(ProjectName, packageSpecDependencies);
 
-            foreach (NuGet.LibraryModel.LibraryDependency package in packages)
+            IList<LibraryDependency> packages = packageSpec.Dependencies;
+
+            foreach (LibraryDependency package in packages)
             {
                 var set = new Model.PackageSet();
                 set.PackageId = new Model.PackageId(package.Name, package.LibraryRange.VersionRange.OriginalString);
                 result.Packages.Add(set);
                 result.Dependencies.Add(set.PackageId);
             }
+
             return result;
+        }
+
+        public JObject ExtractPackageSpecDependencies(string projectJsonPath, ProjectJsonResolver projectJsonResolver)
+        {
+            const string TargetKey = "dependencies";
+            JObject jsonObject = JObject.Parse(File.ReadAllText(projectJsonPath));
+
+            JObject dependenciesObject = projectJsonResolver.FindDependencies(jsonObject, TargetKey);
+
+            JObject packageSpecDependencies = new JObject();
+            packageSpecDependencies.Add(TargetKey, dependenciesObject);
+
+            return packageSpecDependencies;
+        }
+
+        public PackageSpec CreatePackageSpecFromJson(string projectName, JObject packageSpecJsonObject)
+        {
+            string tempFilePath = Path.GetTempFileName();
+            File.WriteAllText(tempFilePath, packageSpecJsonObject.ToString());
+
+            PackageSpec packageSpec = JsonPackageSpecReader.GetPackageSpec(projectName, tempFilePath);
+
+            File.Delete(tempFilePath);
+
+            return packageSpec;
+        }
+        
+        public JObject FindDependencies(JObject jsonObject, string targetKey)
+        {
+            foreach (var property in jsonObject.Properties())
+            {
+                if (property.Name.Equals(targetKey, StringComparison.OrdinalIgnoreCase) && property.Value is JObject)
+                {
+                    return (JObject)property.Value;
+                }
+
+                if (property.Value is JObject nestedObject)
+                {
+                    var result = FindDependencies(nestedObject, targetKey);
+                    if (result != null)
+                    {
+                        return result;
+                    }
+                }
+            }
+
+            return null;
         }
     }
 }
