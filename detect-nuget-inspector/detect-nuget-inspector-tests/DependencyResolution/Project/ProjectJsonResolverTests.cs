ad0ea1e340447b82183b4ed54063765f173604c7
jraihan <jahid.raihan@dsinnovators.com>
2023-06-07 21:59:15 +0600

    added clear namespace. reorganized the code for better readability

--- detect-nuget-inspector/detect-nuget-inspector-tests/DependencyResolution/Project/ProjectJsonResolverTests.cs
+++ detect-nuget-inspector/detect-nuget-inspector-tests/DependencyResolution/Project/ProjectJsonResolverTests.cs
@@ -1,8 +1,6 @@
-using Microsoft.VisualStudio.TestPlatform.Common.Utilities;
 using Newtonsoft.Json.Linq;
-using Synopsys.Detect.Nuget.Inspector.DependencyResolution.Project;
 
-namespace detect_nuget_inspector_tests.DependencyResolution.Project;
+namespace Synopsys.Detect.Nuget.Inspector.DependencyResolution.Project.Test;
 
 [TestClass]
 public class ProjectJsonResolverTests
@@ -14,7 +12,7 @@ public class ProjectJsonResolverTests
 
         return jsonFilePath;
     }
-    
+
     [TestMethod]
     public void ExtractPackageSpecDependenciesTestForOldFormat()
     {
@@ -64,6 +62,7 @@ public class ProjectJsonResolverTests
 
         projectJsonResolver.ExtractPackageSpecDependencies(projectJsonPath, projectJsonResolver);
     }
+
     [TestMethod]
     public void CreatePackageSpecFromJsonTest()
     {
@@ -73,21 +72,22 @@ public class ProjectJsonResolverTests
                     ""Dependency2"": ""4.5.6""
                   }
         }");
-        
+
         string fakeJsonPath = GetJsonPath("fake.json");
         File.WriteAllText(fakeJsonPath, "{}");
-        
+
         ProjectJsonResolver projectJsonResolver = new ProjectJsonResolver("projectName", fakeJsonPath);
         var result = projectJsonResolver.CreatePackageSpecFromJson("projectName", packageSpecJsonObject);
-        
+
         Assert.AreEqual("projectName", result.Name.ToString());
         Assert.AreEqual("1.0.0", result.Version.ToString());
         Assert.AreEqual(2, result.Dependencies.Count);
-       
+
         Assert.IsTrue(result.Dependencies.Any(dependency => dependency.Name == "Dependency2"));
-        
-        File.Delete(fakeJsonPath); 
+
+        File.Delete(fakeJsonPath);
     }
+
     [TestMethod]
     public void ProcessTestForNewFormat()
     {
@@ -103,8 +103,9 @@ public class ProjectJsonResolverTests
 
         Assert.AreEqual(5, result.Packages.Count);
         Assert.AreEqual(5, result.Dependencies.Count);
-        
-        CollectionAssert.Contains(result.Dependencies.Select(dep => dep.Name).ToList(), "Microsoft.AspNetCore.Server.Kestrel");
+
+        CollectionAssert.Contains(result.Dependencies.Select(dep => dep.Name).ToList(),
+            "Microsoft.AspNetCore.Server.Kestrel");
     }
 
     [TestMethod]
@@ -122,10 +123,11 @@ public class ProjectJsonResolverTests
 
         Assert.AreEqual(8, result.Packages.Count);
         Assert.AreEqual(8, result.Dependencies.Count);
-        
-        CollectionAssert.Contains(result.Dependencies.Select(dep => dep.Name).ToList(), "System.Collections.NonGeneric");
+
+        CollectionAssert.Contains(result.Dependencies.Select(dep => dep.Name).ToList(),
+            "System.Collections.NonGeneric");
     }
-    
+
     [TestMethod]
     [ExpectedException(typeof(NullReferenceException))]
     public void ProcessTestWhenDependenciesNotFound()
