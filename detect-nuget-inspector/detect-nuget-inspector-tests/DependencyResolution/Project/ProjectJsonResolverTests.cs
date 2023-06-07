9df5b248af1b19d09ff01f6684fd5b0b845357d5
jraihan <jahid.raihan@dsinnovators.com>
2023-06-07 19:28:31 +0600

    added unit tests for Process method.

--- detect-nuget-inspector/detect-nuget-inspector-tests/DependencyResolution/Project/ProjectJsonResolverTests.cs
+++ detect-nuget-inspector/detect-nuget-inspector-tests/DependencyResolution/Project/ProjectJsonResolverTests.cs
@@ -19,31 +19,35 @@ public class ProjectJsonResolverTests
     {
         string projectJsonPath = GetJsonPath("oldFormat_Project.json");
         var projectJsonResolverForOldFormat = new ProjectJsonResolver("ProjectName", projectJsonPath);
-        JObject result = projectJsonResolverForOldFormat.ExtractPackageSpecDependencies(projectJsonPath, projectJsonResolverForOldFormat);
+        JObject result =
+            projectJsonResolverForOldFormat.ExtractPackageSpecDependencies(projectJsonPath,
+                projectJsonResolverForOldFormat);
 
         Assert.IsNotNull(result);
         Assert.IsTrue(result.ContainsKey("dependencies"));
 
         JObject dependenciesObject = result.GetValue("dependencies") as JObject;
-        
+
         Assert.IsNotNull(dependenciesObject);
         Assert.AreEqual(8, dependenciesObject.Count);
         Assert.AreEqual("4.0.1", dependenciesObject.GetValue("System.Xml.XmlDocument").ToString());
         Assert.AreEqual("4.1.0", dependenciesObject.GetValue("System.AppContext").ToString());
     }
-    
+
     [TestMethod]
     public void ExtractPackageSpecDependenciesTestForNewFormat()
     {
         string projectJsonPath = GetJsonPath("newFormat_Project.json");
         var projectJsonResolverForNewFormat = new ProjectJsonResolver("ProjectName", projectJsonPath);
-        JObject result = projectJsonResolverForNewFormat.ExtractPackageSpecDependencies(projectJsonPath, projectJsonResolverForNewFormat);
-        
+        JObject result =
+            projectJsonResolverForNewFormat.ExtractPackageSpecDependencies(projectJsonPath,
+                projectJsonResolverForNewFormat);
+
         Assert.IsNotNull(result);
         Assert.IsTrue(result.ContainsKey("dependencies"));
 
         JObject dependenciesObject = result.GetValue("dependencies") as JObject;
-     
+
         Assert.IsNotNull(dependenciesObject);
         Assert.AreEqual(5, dependenciesObject.Count);
         Assert.AreEqual("1.0.0", dependenciesObject.GetValue("Microsoft.AspNetCore.Server.IISIntegration").ToString());
@@ -58,5 +62,54 @@ public class ProjectJsonResolverTests
         var projectJsonResolver = new ProjectJsonResolver("ProjectName", projectJsonPath);
 
         projectJsonResolver.ExtractPackageSpecDependencies(projectJsonPath, projectJsonResolver);
+    }
+
+    [TestMethod]
+    public void ProcessTestForNewFormat()
+    {
+        string projectName = "ProjectName";
+        string projectJsonPath = GetJsonPath("newFormat_Project.json");
+        ProjectJsonResolver projectJsonResolver = new ProjectJsonResolver(projectName, projectJsonPath);
+
+        var result = projectJsonResolver.Process();
+
+        Assert.IsNotNull(result);
+        Assert.IsNotNull(result.Packages);
+        Assert.IsNotNull(result.Dependencies);
+
+        Assert.AreEqual(5, result.Packages.Count);
+        Assert.AreEqual(5, result.Dependencies.Count);
+        
+        CollectionAssert.Contains(result.Dependencies.Select(dep => dep.Name).ToList(), "Microsoft.AspNetCore.Server.Kestrel");
+    }
+
+    [TestMethod]
+    public void ProcessTestForOldFormat()
+    {
+        string projectName = "ProjectName";
+        string projectJsonPath = GetJsonPath("oldFormat_Project.json");
+        ProjectJsonResolver projectJsonResolver = new ProjectJsonResolver(projectName, projectJsonPath);
+
+        var result = projectJsonResolver.Process();
+
+        Assert.IsNotNull(result);
+        Assert.IsNotNull(result.Packages);
+        Assert.IsNotNull(result.Dependencies);
+
+        Assert.AreEqual(8, result.Packages.Count);
+        Assert.AreEqual(8, result.Dependencies.Count);
+        
+        CollectionAssert.Contains(result.Dependencies.Select(dep => dep.Name).ToList(), "System.Collections.NonGeneric");
+    }
+    
+    [TestMethod]
+    [ExpectedException(typeof(NullReferenceException))]
+    public void ProcessTestWhenDependenciesNotFound()
+    {
+        string projectName = "ProjectName";
+        string projectJsonPath = GetJsonPath("withoutDependencies_Project.json");
+        ProjectJsonResolver projectJsonResolver = new ProjectJsonResolver(projectName, projectJsonPath);
+
+        var result = projectJsonResolver.Process();
     }
 }
