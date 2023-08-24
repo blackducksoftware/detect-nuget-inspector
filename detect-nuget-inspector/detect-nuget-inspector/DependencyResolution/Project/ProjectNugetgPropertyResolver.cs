728e3229e42a5af45db2510c287250d280593cab
Madhu Gopanna <madhusudan.gopanna@synopsys.com>
2023-08-24 01:16:15 -0400

    ER IDETECT-3046 - Add support for [project-name].nuget.g.props to extract referenced Project asset JSON file path.

---
+++ detect-nuget-inspector/detect-nuget-inspector/DependencyResolution/Project/ProjectNugetgPropertyResolver.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using Synopsys.Detect.Nuget.Inspector.DependencyResolution.Nuget;
+
+namespace Synopsys.Detect.Nuget.Inspector.DependencyResolution.Project
+{
+    class ProjectNugetgPropertyResolver : PropertyResolver
+    {
+
+        private string PropertyPath;
+        private NugetSearchService NugetSearchService;
+
+        public ProjectNugetgPropertyResolver(string propertyPath, NugetSearchService nugetSearchService)
+        {
+            PropertyPath = propertyPath;
+            NugetSearchService = nugetSearchService;
+        }
+
+        public string Process()
+        {
+            var tree = new NugetTreeResolver(NugetSearchService);
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(PropertyPath);
+
+            XmlNodeList projectAssetsFileNodes = doc.GetElementsByTagName("ProjectAssetsFile");
+            if (projectAssetsFileNodes != null && projectAssetsFileNodes.Count > 0)
+            {
+                foreach (XmlNode projectAssetsFileNode in projectAssetsFileNodes)
+                {
+                    if (projectAssetsFileNode.NodeType != XmlNodeType.Comment)
+                    {
+                        return projectAssetsFileNode.InnerText;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
