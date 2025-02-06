dc5c0a892c0ee4189c3899eff63a5948adfb06f8
devmehtabd <devmehta@blackduck.com>
2025-02-06 11:43:53 -0500

    Remove output paths from container class

--- detect-nuget-inspector/detect-nuget-inspector/Inspection/Model/Container.cs
+++ detect-nuget-inspector/detect-nuget-inspector/Inspection/Model/Container.cs
@@ -12,7 +12,6 @@ namespace Blackduck.Detect.Nuget.Inspector.Model
         public string Version { get; set; }
         public string Type { get; set; } = "Solution";
         public string SourcePath { get; set; }
-        public List<string> OutputPaths { get; set; } = new List<string>();
         public List<PackageSet> Packages { get; set; } = new List<PackageSet>();
         public List<PackageId> Dependencies { get; set; } = new List<PackageId>();
         public List<Container> Children { get; set; } = new List<Container>();
