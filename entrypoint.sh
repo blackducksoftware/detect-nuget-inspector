c5e0880739c977de569630601d25847f75a93d37
devmehtabd <devmehta@blackduck.com>
2025-08-12 13:03:33 -0400

    Change entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@ set -e
 PYTHON_PYPI_URL="$1"
 OUTPUT_FILE_NAME="$2"
 
-python3 -m venv ./environment \
- && . ./environment/bin/activate \
- && pip3 install --upgrade pip "$PYTHON_PYPI_URL" \
- && pip3 install SIG-macOS-codesigning "$PYTHON_PYPI_URL"
+python3 -m venv ./environment
+. ./environment/bin/activate
+pip3 install --upgrade pip "$PYTHON_PYPI_URL"
+pip3 install SIG-macOS-codesigning "$PYTHON_PYPI_URL"
 
 echo "Start signing the zip project"
 
