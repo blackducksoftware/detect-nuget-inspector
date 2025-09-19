99911dc22d2f9e1abc48521b5f6f9439b8a86fb6
shantyk <shanty@blackduck.com>
2025-09-19 05:58:22 -0600

    Clean up whitespace changes from diff

--- detect-nuget-inspector/detect-nuget-inspector-tests/Inspection/Util/SolutionDirectoryPackagesPropertyLoaderTests.cs
+++ detect-nuget-inspector/detect-nuget-inspector-tests/Inspection/Util/SolutionDirectoryPackagesPropertyLoaderTests.cs
@@ -14,12 +14,12 @@ namespace detect_nuget_inspector_tests.Inspection.Util
 
             return filePath;
         }
-
+        
         [TestMethod]
         public void ParseStandardDirectoryPackagesPropsFile()
         {
-            string propertyPath = GetFilePath("Standard_Directory.Packages.props");
-
+            string propertyPath = GetFilePath("Standard_Directory.Packages.props"); 
+            
             var solutionDirectoryPackagesPropertyLoader =
                 new SolutionDirectoryPackagesPropertyLoader(GetFilePath(propertyPath), "NONE");
 
@@ -28,47 +28,47 @@ namespace detect_nuget_inspector_tests.Inspection.Util
 
             Assert.IsNotNull(packageVersions);
             Assert.AreEqual(22, packageVersions.Count);
-            Assert.AreEqual(true, versionOverrideEnabled);
-            Assert.AreEqual("2.88.6", packageVersions.First(pkg => pkg.Name.Equals("SkiaSharp.Views.Uno.WinUI")).Version);
-            Assert.AreEqual("4.6.0", packageVersions.First(pkg => pkg.Name.Equals("Microsoft.CodeAnalysis.CSharp")).Version);
-            Assert.AreEqual("1.1.2-beta1.23357.1", packageVersions.First(pkg => pkg.Name.Equals("Microsoft.CodeAnalysis.CSharp.CodeFix.Testing.XUnit")).Version);
+            Assert.AreEqual(true,versionOverrideEnabled);
+            Assert.AreEqual("2.88.6",packageVersions.First(pkg => pkg.Name.Equals("SkiaSharp.Views.Uno.WinUI")).Version);
+            Assert.AreEqual("4.6.0",packageVersions.First(pkg => pkg.Name.Equals("Microsoft.CodeAnalysis.CSharp")).Version);
+            Assert.AreEqual("1.1.2-beta1.23357.1",packageVersions.First(pkg => pkg.Name.Equals("Microsoft.CodeAnalysis.CSharp.CodeFix.Testing.XUnit")).Version);
         }
 
         [TestMethod]
         public void ParseDirectoryPackagesPropsFileWithCpmDisabled()
         {
             string propertyPath = GetFilePath("CPM_Disabled_Directory.Packages.props");
-
+            
             var solutionDirectoryPackagesPropertyLoader =
-                new SolutionDirectoryPackagesPropertyLoader(GetFilePath(propertyPath), "NONE");
-
+                new SolutionDirectoryPackagesPropertyLoader(GetFilePath(propertyPath),"NONE");
+            
             StringWriter stringWriter = new StringWriter();
             Console.SetOut(stringWriter);
-
+            
             HashSet<PackageId> packageVersions = solutionDirectoryPackagesPropertyLoader.Process();
-
+            
             Assert.AreEqual(0, packageVersions.Count);
-            Assert.AreEqual("The user has disabled Central Package Management. Will skip parsing over this file\n", stringWriter.ToString());
+            Assert.AreEqual( "The user has disabled Central Package Management. Will skip parsing over this file\n",stringWriter.ToString());
         }
 
         [TestMethod]
         public void ParseDirectoryPackagesPropsFileWithVersionOverrideDisabled()
         {
             string propertyPath = GetFilePath("VersionOverride_Disabled_Directory.Packages.props");
-
+            
             var solutionDirectoryPackagesPropertyLoader =
                 new SolutionDirectoryPackagesPropertyLoader(GetFilePath(propertyPath), "NONE");
-
+            
             bool versionOverrideEnabled = solutionDirectoryPackagesPropertyLoader.GetVersionOverrideEnabled();
-
+            
             Assert.AreEqual(false, versionOverrideEnabled);
         }
-
+        
         [TestMethod]
         public void ParseDirectoryPackagesPropsFileWithGlobalPackageReferences()
         {
             string propertyPath = GetFilePath("GlobalPackageReference_Directory.Packages.props");
-
+            
             var solutionDirectoryPackagesPropertyLoader =
                 new SolutionDirectoryPackagesPropertyLoader(GetFilePath(propertyPath), "NONE");
 
@@ -76,11 +76,11 @@ namespace detect_nuget_inspector_tests.Inspection.Util
 
             HashSet<PackageId> globalPackageReferences =
                 solutionDirectoryPackagesPropertyLoader.GetGlobalPackageReferences();
-
-            Assert.AreEqual(19, packageVersions.Count);
-            Assert.AreEqual(7, globalPackageReferences.Count);
-            Assert.AreEqual("1.2.0.507", globalPackageReferences.First(pkg => pkg.Name.Equals("StyleCop.Analyzers.Unstable")).Version);
-            Assert.AreEqual("1.1.1", globalPackageReferences.First(pkg => pkg.Name.Equals("Microsoft.SourceLink.GitHub")).Version);
+            
+            Assert.AreEqual(19,packageVersions.Count);
+            Assert.AreEqual(7,globalPackageReferences.Count);
+            Assert.AreEqual("1.2.0.507",globalPackageReferences.First(pkg => pkg.Name.Equals("StyleCop.Analyzers.Unstable")).Version);
+            Assert.AreEqual("1.1.1",globalPackageReferences.First(pkg => pkg.Name.Equals("Microsoft.SourceLink.GitHub")).Version);
         }
         
         [TestMethod]
